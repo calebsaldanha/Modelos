faa8b8e6ef5a4bd9a8794a9fca613e3127132d28
Caleb <calebsaldanhawork@gmail.com>
2025-03-25 19:21:37 -0300

    Add files via upload

--- automl_diabetes_training/src/inference.py
+++ automl_diabetes_training/src/inference.py
@@ -1,1 +1,22 @@
+import joblib
+import pandas as pd
+import numpy as np
 
+# Carregar o modelo
+model = joblib.load('model/model.pkl')
+
+# Nova entrada para previsão
+input_data = pd.DataFrame({
+    "Pregnancies": [1],
+    "PlasmaGlucose": [85],
+    "DiastolicBloodPressure": [66],
+    "TricepsThickness": [29],
+    "SerumInsulin": [0],
+    "BMI": [26.6],
+    "DiabetesPedigree": [0.351],
+    "Age": [34]
+})
+
+# Realizar previsão
+prediction = model.predict(input_data)
+print(f'Prediction: {prediction}')
--- automl_diabetes_training/src/train_model.py
+++ automl_diabetes_training/src/train_model.py
@@ -1,1 +1,21 @@
+from azureml.core import Workspace, Dataset
+from azureml.train.automl import AutoMLConfig
+from azureml.core.experiment import Experiment
 
+# Carregar dados
+ws = Workspace.from_config()
+dataset = Dataset.get_by_name(ws, name='diabetes_dataset')
+train_data = dataset.to_pandas_dataframe()
+
+# Configuração do AutoML
+automl_config = AutoMLConfig(
+    task='classification',
+    primary_metric='accuracy',
+    X=train_data.drop(columns=['target']),
+    y=train_data['target'],
+    n_cross_validations=5
+)
+
+# Executar o experimento
+experiment = Experiment(ws, 'diabetes_experiment')
+run = experiment.submit(automl_config)
